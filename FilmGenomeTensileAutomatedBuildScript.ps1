b1b772fcda5483a25474f9778557af62911efc1c
Watson-Morris, Morgan (M) <mwatsonmorris@dow.com>
2017-06-14 14:44:41 -0400

    added Automated build PS script and newer GitUserInfo PS  spript

---
+++ FilmGenomeTensileAutomatedBuildScript.ps1
@@ -0,0 +1,103 @@
+﻿#Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+
+
+
+
+#get credentials from user for later use 
+#$creds = Get-Credential -UserName "SDIRROBOTICSTFS" -Message "Please Enter a Username and Password for Authentication to TFS 2017"
+
+
+
+#validate credentials against active directory to ensure they will work
+#$Domain = $env:USERDOMAIN
+#$ct = [System.DirectoryServices.AccountManagement.ContextType]::Domain
+#$pc = New-Object System.DirectoryServices.AccountManagement.PrincipalContext $ct,$Domain
+#$valid = $pc.ValidateCredentials($creds.UserName,$creds.GetNetworkCredential().Password)
+
+if (-not $valid){
+    
+    #$Override = (New-Object -ComObject Wscript.Shell).Popup("These Credentials Are not in Active Directory! If you are sure they will authenticate to TFS click 'Ok'",0,"Done",0x1)
+
+    if ($Override -ne 1){
+        #Write-Host "These Credentials Are not in Active Directory and the User has chosen to Exit the Script. Please Use Credentials that are Valid for access."
+        #return
+    } 
+    
+} else {
+    #Write-Host "These Credentials Are in Active Directory, and will hopefully authenticate to TFS."
+}
+
+
+
+#check for git
+Write-Host "Checking for Git install..."
+$gitvs = git --version
+$gitinst = $gitvs -like "git version *"
+if (-not $gitinst){
+    Write-Host "git is not currently installed"
+    [System.Windows.Forms.MessageBox]::Show("Error" , "No Git Installers Found. Please Install Git!")
+    return
+
+
+    #extra code for if I ever feel like trying to automate git install
+    #good sample for stuff though
+
+    Write-Host "git is not currently installed, checking for the installer..."
+    $localuserdirs = ls "C:\Users\" .\ -Include "u*"
+    $gitexefiles = @()
+    $localuserdirs | ForEach-Object{
+        $dirstring = "C:\Users\" + $_.Name + "\Downloads\"
+        $gitexefiles += ls $dirstring -Filter "Git*.exe"
+    }
+
+    if (-not $gitexefiles){
+        Write-Host "No Git Installers Found"
+        [System.Windows.Forms.MessageBox]::Show("No Git Installers Found. Please Install Git!" , "Error")
+        return
+    }
+
+    Write-Host "No Git Installer(s) Found"
+    Write-Host "Determining Most Recently Installed"
+}
+
+#Take Care of the  implementer software
+$implementerREPOURL = "https://rndtfs.intranet.dow.com/dow/IR/_git/FilmGenomeTensileImplementer"
+$implementerpath = "C:\Code"
+$implementerpathexists = Test-Path $implementerpath
+if (-not $implementerpathexists){
+    Write-Host "Implementer Path " + $implementerpath + " Does not Exist, making it..."
+    New-Item $implementerpath -type directory
+}
+cd $implementerpath
+git clone $implementerREPOURL
+
+#Take care of the EPSON code
+$EPSONREPOURL = "https://rndtfs.intranet.dow.com/dow/IR/_git/FilmGenomeTensileEPSON"
+$epsonpath = "C:\EpsonRC70\projects"
+$epsonpathexists = Test-Path $epsonpath
+if (-not $epsonpathexists){
+    Write-Host "EPSON Path " + $epsonpath + " Does not Exist."
+    [System.Windows.Forms.MessageBox]::Show("EPSON Path (" + $epsonpath + ") was not found, this means the Correct Version RC+ Software Has not been Installed. Please install this first." , "Error")
+    return
+}
+cd $epsonpath
+git clone $EPSONREPOURL
+
+#Take care of the Aerotech code
+$AerotechREPOURL = "https://rndtfs.intranet.dow.com/dow/IR/_git/FilmGenomeTensileAerotech"
+$aerotechpath = "C:\Users\Public\Documents\Aerotech\Ensemble\User Files"
+$aerotechpathexists = Test-Path $aerotechpath
+if (-not $aerotechpathexists){
+    Write-Host "EPSON Path " + $aerotechpath + " Does not Exist."
+    [System.Windows.Forms.MessageBox]::Show("Aerotech Path (" + $aerotechpath + ") was not found, this means the Correct Version Ensemble Software Has not been Installed. Please install this first." , "Error")
+    return
+}
+cd $aerotechpath
+cd $AerotechREPOURL
+
+#Take care of SICK
+$SICKRepo = "SICK Safety"
+$SICKREPOURL = "https://rndtfs.intranet.dow.com/dow/IR/_git/FilmGenomeTensileSICK"
+cd C:\
+git clone $SICKREPOURL
+Rename-Item C:\FilmGenomeTensileSICK $SICKRepo
---
+++ GitUserInfo.ps1
@@ -0,0 +1,77 @@
+﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+
+
+
+#Git Function
+function Set-UserInfo ( [string] $N, [string] $E) {
+    git config --global --unset user.name
+    git config --global --unset user.email
+    git config --global --replace user.name "$N"
+    git config --global --replace user.email "$E"
+}
+
+
+
+#BUild Form
+$objForm = New-Object System.Windows.Forms.Form 
+$objForm.Text = "Git User Info"
+$objForm.Size = New-Object System.Drawing.Size(300,220) 
+$objForm.StartPosition = "CenterScreen"
+
+#set Key actions
+$objForm.KeyPreview = $True
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") 
+    {$x=$objTextBox.Text;$objForm.Close()}})
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") 
+    {$objForm.Close()}})
+
+
+#OK Button
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Size(75,140)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = "OK"
+$OKButton.Add_Click({$Name=$objNameTextBox.Text;$Email=$objEmailTextBox.Text;Set-UserInfo $Name $Email;$objForm.Close()})
+$objForm.Controls.Add($OKButton)
+
+#Cancel Button
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Size(150,140)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = "Cancel"
+$CancelButton.Add_Click({$objForm.Close()})
+$objForm.Controls.Add($CancelButton)
+
+#Name Information
+$objNameLabel = New-Object System.Windows.Forms.Label
+$objNameLabel.Location = New-Object System.Drawing.Size(10,20) 
+$objNameLabel.Size = New-Object System.Drawing.Size(280,20) 
+$objNameLabel.Text = "Please Enter Your Name:"
+$objForm.Controls.Add($objNameLabel) 
+
+$objNameTextBox = New-Object System.Windows.Forms.TextBox 
+$objNameTextBox.Location = New-Object System.Drawing.Size(10,40) 
+$objNameTextBox.Size = New-Object System.Drawing.Size(260,20)
+$objNameTextBox.Text = git config --global --get "user.name" 
+$objForm.Controls.Add($objNameTextBox) 
+
+#Email Information
+$objEmailLabel = New-Object System.Windows.Forms.Label
+$objEmailLabel.Location = New-Object System.Drawing.Size(10,80) 
+$objEmailLabel.Size = New-Object System.Drawing.Size(280,20) 
+$objEmailLabel.Text = "Please Enter Your Email:"
+$objForm.Controls.Add($objEmailLabel) 
+
+$objEmailTextBox = New-Object System.Windows.Forms.TextBox 
+$objEmailTextBox.Location = New-Object System.Drawing.Size(10,100) 
+$objEmailTextBox.Size = New-Object System.Drawing.Size(260,20)
+$objEmailTextBox.Text = git config --global --get "user.email"
+$objForm.Controls.Add($objEmailTextBox) 
+
+
+#Display
+$objForm.Topmost = $True
+
+$objForm.Add_Shown({$objForm.Activate()})
+[void] $objForm.ShowDialog()
